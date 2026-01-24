170163b3d64ad8f69366be8081971603cf830036
Luis Ganeiro <l.gameiro@hotmail.com>
2026-01-24 10:09:44 +0000

    fix: correct import path for GoogleAnalytics component

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Layout } from '@components/common'
 import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from 'next-themes'
 import { PHProvider } from '@lib/analytics/posthogProvider'
-import { GoogleAnalytics } from '@lib/analytics/googleAnalytics'
+import { GoogleAnalytics } from '@lib/analytics/GoogleAnalytics'
 
 export default function RootLayout({
   children,
